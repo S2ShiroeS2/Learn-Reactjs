f43fdd0af454a74baab7005f59987200d493dcb8
S2ShiroeS2 <tuquochung123@gmail.com>
2019-09-29 16:44:58 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ class App extends Component {
   render() {
     return (
         <div className="Shopping-list">
-            <h1>Shopping List for {this.props.name}</h1>
-            <ul>
-                <li>Instagram</li>
-                <li>WhatsApp</li>
-                <li>Oculus</li>
-            </ul>
+            
         </div>
     );
   }
